506728071ac760b7a5250d17448b612d0f8c9919
Vincypriano <83838324+Vincypriano@users.noreply.github.com>
2021-06-20 13:18:17 -0300

    Update ex058#Jogo_advinhação_v.2.0.py

--- ex058#Jogo_advinhação_v.2.0.py
+++ ex058#Jogo_advinhação_v.2.0.py
@@ -6,12 +6,19 @@ from random import randint
 from time import sleep
 cont = 0
 computador = randint(0,10)
-print('Escolha um número de 0 a 10 e tente advinhar qual o computador pensou!!!')
-jogador = int(input('Digite o numero: '))
+print('Escolha um número de 0 a 10 e tente advinhar qual o computador \U0001f5a5\uFE0F  pensou!!!')
+jogador = int(input('\U0001f579\uFE0F  Digite o numero \U0001f3b0 : '))
 
 while computador != jogador:
-    print ('Errou!!! Você digitou {} e o computador pensou {}'.format(jogador,computador))
+    print('\U0001f6a8Errou\U0001f6a8!!!')
+    sleep(1)
+    if jogador > computador:
+        print('Você digitou um numero maior!!',end='')
+        continue
+    if jogador < computador:
+        print('Você digitou um número Menor!!',end='')
+        continue
+    jogador = int(input((' Digite outro Numero: '.format(jogador,computador))))
     cont += 1
-else:
-    print('Você acertou!!!')
-    
+
+print('Você acertou com {} tentativas!!!'.format(cont))
