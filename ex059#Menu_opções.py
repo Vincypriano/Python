641a1cf887fceb4e799f3aadcb82b035479dd8ef
Vincypriano <83838324+Vincypriano@users.noreply.github.com>
2021-06-23 15:09:00 -0300

    ex#59

--- ex059#Menu_opções.py
+++ ex059#Menu_opções.py
@@ -3,11 +3,44 @@ print('\033[36;40m=\033[m' * 46)
 
 num_1 = float(input('Digite o Primeiro Numero: '))
 num_2 = float(input('Digite o Segundo Numero: '))
-print('''Qual operação você gostaria de executar:
-OPÇÔES:
-[1] SOMAR
-[2]MULTIPLICAR
-[3]MAIOR
-[4]NOVOS NÚMEROS
-[5] SAIR DO PROGRAMA''')
-opção = int(input('Qual sua opção: '))
+opção = 0
+while opção != 5:
+    print('''Qual operação você gostaria de executar:
+    OPÇÔES:
+    [1]SOMAR
+    [2]MULTIPLICAR
+    [3]MAIOR
+    [4]NOVOS NÚMEROS
+    [5] SAIR DO PROGRAMA''')
+    opção = int(input('Qual sua opção: '))
+    if opção == 1:
+        print('A Soma dos numeros {} e {} é igual a {}'.format(num_1,num_2,(num_1+num_2)))
+    elif opção == 2:
+        print('A multiplicação dos numeros {} e {} é igual a {}.'.format(num_1,num_2, (num_1*num_2)))
+    elif opção == 3:
+        if num_1 > num_2:
+            num_3 = num_1
+        else:
+            num_3 = num_2
+        print('Entre {} e {} o maior é {}'.format(num_1, num_2,num_3))
+    elif opção == 4:
+        num_1 = float(input('Digite o Primeiro Numero: '))
+        num_2 = float(input('Digite o Segundo Numero: '))
+        print('''Qual operação você gostaria de executar:
+        OPÇÔES:
+        [1]SOMAR
+        [2]MULTIPLICAR
+        [3]MAIOR
+        [4]NOVOS NÚMEROS
+        [5] SAIR DO PROGRAMA''')
+        opção = int(input('Qual sua opção: '))
+    elif opção == 5:
+        print('Fim do Programa')
+    else:        
+        print('Opção inválida. Tente novamente...')
+print('Finalizando...')
+
+
+    
+
+
--- teste001.py
+++ teste001.py
@@ -1,2 +1,23 @@
-frase = str(input('Digite uma frase: ')).title().strip()
-print(frase,len(frase), frase.split(),'-'.join(frase))
+#frase = str(input('Digite uma frase: ')).title().strip()
+#print(frase,len(frase), frase.split(),'-'.join(frase))
+#s = "hello"
+#print('{} and {}'.format(s[len(s)-1],s[-1]))
+'''list = [0,0,0]
+print('{} {} {}'.format(list[0],list[1],list[2]))
+list3 = [1,2,[3,4,'hello']]
+list4 = list3[-1]
+list4[-1] = 'goodbye' 
+list3[-1] = list4
+print(list3)'''
+'''list5 = [8,6,4,3,2,9]
+list5.sort ()
+print(list5)
+d = {'key1':{'key2':'hello'}}
+d2 = d['key1']
+print(d2['key2'])
+'''
+d = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+d2 = d['k1'][0]
+d3 = d2['nest_key']
+d4 = d3[1]
+print (d4)
